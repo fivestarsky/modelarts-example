3db54c3393f0013bb9b5a1fdab6072b7ef87a1f6
fivestarsky <fivestarsky@gmail.com>
2018-10-30 15:59:24 +0800

    example

--- Using AutoML for Bank Marketing Application/example.py
+++ Using AutoML for Bank Marketing Application/example.py
@@ -5,26 +5,31 @@ import zipfile
 
 print("downloading with urllib")
 
-bank_filename = "./bank.zip"
 bank_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00222/bank.zip"
-bank_additional_filename = "./bank-additional.zip"
+bank_filename = "./bank.zip"
+bank_dir = "./bank/"
+
 bank_additional_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00222/bank-additional.zip"
+bank_additional_filename = "./bank-additional.zip"
+bank_additional_dir = "./bank-additional/"
 
 
 with urllib.request.urlopen(bank_url) as web:
     # 为保险起见使用二进制写文件模式，防止编码错误
     with open(bank_filename, 'wb') as outfile:
         outfile.write(web.read())
-
-    f = zipfile.ZipFile(bank_filename,'r')
-    for file in f.namelist():
-        f.extract(file,"./bank/")
+f = zipfile.ZipFile(bank_filename, 'r')
+for file in f.namelist():
+    f.extract(file, bank_dir)
 
 
 with urllib.request.urlopen(bank_additional_url) as web:
     # 为保险起见使用二进制写文件模式，防止编码错误
     with open(bank_additional_filename, 'wb') as outfile:
         outfile.write(web.read())
+f = zipfile.ZipFile(bank_additional_filename, 'r')
+for file in f.namelist():
+    f.extract(file, bank_additional_dir)
 
 
 # print("downloading with " + bank_url)
