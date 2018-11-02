6fa86c5e7b2b067ecc6cf19ba429a46a6261233b
fanliang meng <fivestarsky@163.com>
2018-11-02 16:04:33 +0800

    demo

--- Using AutoML for Bank Marketing Application/example.py
+++ Using AutoML for Bank Marketing Application/example.py
@@ -1,7 +1,8 @@
 import urllib.request
 import zipfile
 
-
+# 引入OBS模块
+from obs import ObsClient
 
 print("downloading with urllib")
 
@@ -31,12 +32,37 @@ for file in f.namelist():
     f.extract(file, extranct_dir)
 
 
-# print("downloading with " + bank_url)
-# LocalPath = os.path.join('./', bank_filename)
-# urllib.request.urlretrieve(bank_url, LocalPath)
-#
-#
-# print("downloading with " + bank_additional_url)
-# LocalPath = os.path.join('./', bank_additional_filename)
-# urllib.request.urlretrieve(bank_additional_url, LocalPath)
-#
+
+bucketName = 'model-arts-demo'
+# 创建ObsClient实例
+with ObsClient(
+    access_key_id='LGCUKNYSCPHOLEH5UPZP',
+    secret_access_key='1g7ttzgdelafd1v6teb1qR2iKqRzJpYQuOwl8rgw',
+    server='https://model-arts-demo.obs.cn-north-1.myhwclouds.com'
+) as obsClient:
+    # 设置桶的多版本状态
+    try:
+        resp = obsClient.createBucket(bucketName)
+        if resp.status < 300:
+            print('requestId:', resp.requestId)
+        else:
+            print('errorCode:', resp.errorCode)
+            print('errorMessage:', resp.errorMessage)
+            raise Exception('创建桶错误')
+
+        resp = obsClient.setBucketVersioning(bucketName, 'Enabled')
+        if resp.status < 300:
+            print('requestId:', resp.requestId)
+        else:
+            print('errorCode:', resp.errorCode)
+            print('errorMessage:', resp.errorMessage)
+            raise Exception('打开桶多版本支持错误')
+    except:
+        import traceback
+        print(traceback.format_exc())
+
+    finally:
+        obsClient.close()
+
+
+
